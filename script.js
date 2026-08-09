786579444c9c27b00e22a25cd7e5bf724d80faa4
Mohammad mahan Abdoli <mahanabdoli2010@gmail.com>
2026-08-09 15:49:19 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let words = document.querySelector('#word');
+words.innerHTML = words.textContent.split(" ").map((word) => `<span class="text">${word}</span>`).join(" ")
+
+let text = document.querySelectorAll(".text");
+
+let i = 0;
+
+setInterval(() => { 
+    text.forEach((span) => span.classList.remove("active"));
+    text[i].classList.add("active");
+    i++;
+    if (i === text.length) {
+    i = 0;
+    }
+},500)
+
+let items = document.querySelectorAll(".items p")
+
+let g = 0
+setInterval(()=> {
+    items.forEach((item) => item.classList.remove("show"))
+    items[g].classList.add("show")
+    g++
+    if(g === items.length) {
+        g = 0;
+    }
+},2000)
